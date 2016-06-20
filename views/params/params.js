20d75e4f7a649818353d0a0ba6cf76e591ec1ffb
Yannis Varni <yannis.varni@laerce.fr>
2016-06-20 12:25:14 +0200

    Mise à jour du module params avec changement des valeurs du service Settings

--- views/params/params.js
+++ views/params/params.js
@@ -1,10 +1,10 @@
 /**
  * Created by hippias on 20/06/2016.
  */
-App.controller('ParamsController', function($scope) {
-    $scope.variableController = "esh";
+App.controller('ParamsController', function($scope, Settings) {
+    $scope.settings = angular.copy(Settings.values);
 
-    $scope.alertUpperCase = function () {
-        alert($scope.variableController.toUpperCase());
+    $scope.save = function () {
+      Settings.values = angular.copy($scope.settings);
     };
 });
