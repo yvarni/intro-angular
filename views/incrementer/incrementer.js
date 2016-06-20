0fd3ae9195726cf345c3816726be207db88ecc46
Yannis Varni <yannis.varni@laerce.fr>
2016-06-20 12:24:55 +0200

    Mise à jour d'incrementer

--- views/incrementer/incrementer.js
+++ views/incrementer/incrementer.js
@@ -1,3 +1,11 @@
-App.controller('IncrementerController', function($scope) {
-    
+App.controller('IncrementerController', function($scope, Settings) {
+    $scope.value = 1;
+
+    $scope.increment = function () {
+        $scope.value += Settings.values.increment;
+    };
+
+    $scope.decrement = function () {
+        $scope.value -= Settings.values.increment;
+    };
 });
