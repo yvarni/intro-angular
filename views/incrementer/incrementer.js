07f8c6f9489fd6fb584ad53ca72811e1b8210fa7
Yannis Varni <yannis.varni@laerce.fr>
2016-06-20 16:36:31 +0200

    Rajout de la fonctionnalité d'historisation des incrémentations

--- views/incrementer/incrementer.js
+++ views/incrementer/incrementer.js
@@ -1,11 +1,22 @@
-App.controller('IncrementerController', function($scope, Settings) {
-    $scope.value = 1;
+App.controller('IncrementerController', function($scope, Settings, $state) {
+    $scope.value = $state.value || 1;
+
+    $scope.history = $state.history || [];
 
     $scope.increment = function () {
         $scope.value += Settings.values.increment;
+        $scope.history.push({ timestamp: Date.now(), value: $scope.value });
+        this.saveState();
     };
 
     $scope.decrement = function () {
         $scope.value -= Settings.values.increment;
+        $scope.history.push({ timestamp: Date.now(), value: $scope.value });
+        $scope.saveState();
     };
+
+    $scope.saveState = function () {
+        $state.value = $scope.value;
+        $state.history = angular.copy($scope.history);
+    }
 });
