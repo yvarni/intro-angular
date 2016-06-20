9e15792b2392c29fbe1c81c7d96b707d350cac06
Yannis Varni <yannis.varni@laerce.fr>
2016-06-20 11:42:27 +0200

    Mise en place du module parametres

--- views/params/params.js
+++ views/params/params.js
@@ -2,5 +2,9 @@
  * Created by hippias on 20/06/2016.
  */
 App.controller('ParamsController', function($scope) {
-    $scope.test = "esh";
+    $scope.variableController = "esh";
+
+    $scope.alertUpperCase = function () {
+        alert($scope.variableController.toUpperCase());
+    };
 });
